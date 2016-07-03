e66ff7bfd58a0cac62e47fd1a9e2e870bf6f6be3
Alka Gaikwad <alka.gaikwad@dtechnologies.co.in>
2016-07-03 09:56:59 +0530

    Add weather details view controller cell to display 14 days data on table view

---
+++ iOSWeatherApplication/WeatherDetailsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherDetailsTableViewCell.swift
+//  iOSWeatherApplication
+//
+//  Created by Kingfisher on 7/3/16.
+//  Copyright © 2016 dahlia. All rights reserved.
+//
+
+import UIKit
+
+class WeatherDetailsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
